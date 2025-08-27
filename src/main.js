4c218d00babeeb3e33d0e458cd62e9316686d7be
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2025-08-26 22:54:06 -0300

    build: remove dompurify

--- src/main.js
+++ src/main.js
@@ -2,8 +2,5 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './routes/router';
 import store from './store';
-import { setupTrustedTypes } from './trusted-types';
-
-setupTrustedTypes();
 
 createApp(App).use(router).use(store).mount('#app');
