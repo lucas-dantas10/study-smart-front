6b6a3c57143cb883e88855b34ea04cd75cd66d89
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2025-08-26 22:35:04 -0300

    feat: add dompurify

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './routes/router';
 import store from './store';
+import { setupTrustedTypes } from './trusted-types';
+
+setupTrustedTypes();
 
 createApp(App).use(router).use(store).mount('#app');
